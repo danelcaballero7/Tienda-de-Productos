bbda7c00411e0d2e98c866a24b3f90e3b3810a7f
danelcaballero7 <danelcaballero7@gmail.com>
2022-03-15 22:31:49 -0700

    segundo commit

---
+++ Tiendaapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, "Tiendaapp/home.html")
+
+def servicios(request):
+    return render(request, "Tiendaapp/servicios.html")
+
+def tienda(request):
+    return render(request, "Tiendaapp/tienda.html")
+
+def blog(request):
+    return render(request, "Tiendaapp/blog.html")
+
+def contacto(request):
+    return render(request, "Tiendaapp/contacto.html")
