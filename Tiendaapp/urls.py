e3cc87f950af66134fe7aacf1b59a6419940d783
danelcaballero7 <danelcaballero7@gmail.com>
2022-04-11 08:48:08 -0700

    segundo commit

--- Tiendaapp/urls.py
+++ Tiendaapp/urls.py
@@ -11,7 +11,5 @@ urlpatterns = [
     path('blog/', views.blog, name='Blog'),
     path('contacto/', views.contacto, name="Contacto"),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
